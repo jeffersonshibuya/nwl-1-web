0dff5742874fc9d2cb3e1a9e053bce6dfe4a8ae2
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-07-20 15:48:48 -0300

    hotjar in app config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { hotjar } from 'react-hotjar'
 import "./App.css";
 
 import Routes from './routes'
 
 function App() {
+  useEffect(() => {
+    hotjar.initialize(1909963, 6)
+  }, [])
+
   return <Routes />;
 }
 
